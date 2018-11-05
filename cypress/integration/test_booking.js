06880806918c25a2dc4ae964454c4a9c1002d1b0
WNjihia <dorisn172@gmail.com>
2018-11-05 17:24:34 +0300

    add search hotels spec

--- cypress/integration/test_booking.js
+++ cypress/integration/test_booking.js
@@ -8,6 +8,19 @@ describe('Testing Hotel Booking Process', function () {
     cy.url().should('include', 'booking.com')
   });
 
-  
+  it('Searches for available hotels', () => {
+    cy.get('#ss').type('London').should('have.value', 'London');
+    cy.get('.sb-autocomplete__badge.sb-autocomplete__badge--popular')
+      .first().click();
+    cy.get('.bui-calendar__wrapper')
+      .last()
+      .find('tbody>tr')
+      .last()
+      .find('td')
+      .first().click()
+      .next().click();
+    cy.get('.sb-searchbox__button').click();
+    cy.url().should('include', 'booking.com/searchresults');
+  });
 
   });
